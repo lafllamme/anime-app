ab723d36c2c3755a9fba98e15dc2405b67fb42ab
Dogi <flamebook@Dogis-MacBook-Pro.fritz.box>
2022-03-19 13:40:42 +0100

    adds login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <NuxtLayout name="navbar">
-    <body>
+  <div>
+    <NuxtLayout name="navbar">
       <div
         class="hero min-h-screen"
-        style="
-          background-image: url(https://pic.re/image);
-        "
+        style="background-image: url(https://pic.re/image)"
       >
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
@@ -15,12 +13,15 @@
               Willkommen zu unseren neuen Plattform für Animes!<br />
               Du findest hier alles zu neue Folgen, Anime-Serien oder Mangas.
             </p>
-            <button class="btn btn-primary">Los Geht's</button>
+            <button class="btn btn-primary">
+              <NuxtLink to="/login" :prefetch="true">Los Geht's</NuxtLink>
+            </button>
           </div>
         </div>
       </div>
-    </body>
-  </NuxtLayout>
+    </NuxtLayout>
+    <NuxtLayout name="footer"></NuxtLayout>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ pages/login/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="hero min-h-screen bg-base-200"
+    style="background-image: url(https://pic.re/image)"
+  >
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="text-center lg:text-left bg-base-200 opacity-90 rounded-lg">
+        <h1 class="text-5xl font-bold text-center">Log dich ein!</h1>
+        <p class="py-6 text-center">
+          Du hast bei uns alles auf einen Überblick. Deine angeschauten Animes
+          werden automatisch in deinem Homefeed angezeigt.
+        </p>
+        <p class="py-6 text-center">Noch nicht angemeldet? Dann klick hier</p>
+      </div>
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="email"
+              class="input input-bordered"
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="text"
+              placeholder="password"
+              class="input input-bordered"
+            />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover"
+                >Forgot password?</a
+              >
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
